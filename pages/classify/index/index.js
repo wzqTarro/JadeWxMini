38ec8d0828f10442590cab4c180ae9b7df3c2c96
wzq <sdwzq@outlook.com>
2019-04-25 23:26:30 +0800

    分类页面

---
+++ pages/classify/index/index.js
@@ -0,0 +1,121 @@
+const app = getApp()
+Component({
+  options: {
+    // 引用全局样式
+    addGlobalClass: true
+  },
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    Custom: app.globalData.Custom,
+    TabCur: 0,
+    MainCur: 0,
+    VerticalNavTop: 0,
+    list: [],
+    load: true,
+    // 九宫格
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: 'VR'
+    }, {
+      icon: 'recordfill',
+      color: 'orange',
+      badge: 1,
+      name: '录像'
+    }, {
+      icon: 'picfill',
+      color: 'yellow',
+      badge: 0,
+      name: '图像'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '通知'
+    }, {
+      icon: 'upstagefill',
+      color: 'cyan',
+      badge: 0,
+      name: '排行榜'
+    }, {
+      icon: 'clothesfill',
+      color: 'blue',
+      badge: 0,
+      name: '皮肤'
+    }],
+    gridCol: 3,
+    skin: false
+  },
+  lifetimes: {
+    attached() {
+      // 每次页面展示都会触发
+      wx.showLoading({
+        title: '加载中...',
+        mask: true
+      });
+      let list = [{}];
+      for (let i = 0; i < 1; i++) {
+        list[i] = {};
+        list[i].name = "和田碧玉";
+        list[i].id = i;
+      }
+      this.setData({
+        list: list,
+        listCur: list[0]
+      })
+    },
+    ready(){
+      wx.hideLoading()
+    }
+  },
+  methods: {
+    goClassify(){
+      wx.navigateTo({
+        url: '/pages/classify/classify/classify'
+      })
+    },
+    tabSelect(e) {
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        MainCur: e.currentTarget.dataset.id,
+        VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+      })
+    },
+    VerticalMain(e) {
+      let that = this;
+      let list = this.data.list;
+      let tabHeight = 0;
+      if (this.data.load) {
+        for (let i = 0; i < list.length; i++) {
+          let view = wx.createSelectorQuery().select("#main-" + list[i].id);
+          view.fields({
+            size: true
+          }, data => {
+            list[i].top = tabHeight;
+            tabHeight = tabHeight + data.height;
+            list[i].bottom = tabHeight;
+          }).exec();
+        }
+        that.setData({
+          load: false,
+          list: list
+        })
+      }
+      let scrollTop = e.detail.scrollTop + 20;
+      for (let i = 0; i < list.length; i++) {
+        if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+          that.setData({
+            VerticalNavTop: (list[i].id - 1) * 50,
+            TabCur: list[i].id
+          })
+          return false
+        }
+      }
+    }
+  }
+  
+})
